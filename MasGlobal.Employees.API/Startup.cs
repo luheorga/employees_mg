7b2ff29e0a484bdbb36b0a479d4525bd917d1f60
Luis Orjuela <lhorjuelag@hotmail.com>
2019-07-08 13:38:49 -0500

    Angular app added to API startup

--- MasGlobal.Employees.API/Startup.cs
+++ MasGlobal.Employees.API/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using MasGlobal.Employees.Resolver;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MasGlobal.Employees.API
@@ -9,13 +12,30 @@ namespace MasGlobal.Employees.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvcCore()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddJsonFormatters();
+
             services.AddEmployeeServices();
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "App/dist";
+            });
         }
         
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSpaStaticFiles();
             app.UseMvc();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "App";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
         }
     }
 }
